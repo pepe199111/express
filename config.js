2578b789dc6fb1d565b35cee8e9180e9345475f1
Piotr Pustul <pepe@Piotrs-MacBook-Pro.local>
2020-12-09 18:10:57 +0000

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: '',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+}
+
+
+// 'mongodb+srv://admin:iNJUipK59D3dySPb@cluster0.ayjah.mongodb.net/test?retryWrites=true&w=majority',
