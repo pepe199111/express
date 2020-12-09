e8d45a1ca64dd1927c89e1d61d03ebeea7ef3ae6
Piotr Pustul <pepe@Piotrs-MacBook-Pro.local>
2020-12-09 18:53:30 +0000

    news filter front

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://admin:iNJUipK59D3dySPb@cluster0.ayjah.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000,
 }
-
-
-// 'mongodb+srv://admin:iNJUipK59D3dySPb@cluster0.ayjah.mongodb.net/test?retryWrites=true&w=majority',
